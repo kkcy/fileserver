5b92a54cbc92610ea8ba3e74a7617de11cc82a7a
kkcy <rickykkcy@gmail.com>
2018-02-05 16:24:05 +0800

    Create loadee_standard.py

---
+++ loadee_standard.py
@@ -0,0 +1,17 @@
+import chirpsdk
+import sys
+import threading
+import time
+
+sdk = chirpsdk.ChirpSDK('iRxy7msaMmn2xw3DG7TRxvnzA', 'cJJMu9ujBJ3WF1EE8z9pgh9HuykZbfkB0qPXw6cQ4kR1ppfxu8')
+
+# sdk.set_protocol('ultrasonic')
+# chirp = sdk.create_chirp(sys.argv[1])
+chirp = sdk.create_chirp()
+
+# print chirp.identifier
+sdk.streaming_interval = 500
+sdk.start_streaming(chirp)
+
+while True:
+	time.sleep(100)
